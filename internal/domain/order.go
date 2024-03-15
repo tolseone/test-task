ffce893e09ede6a1de4b1cf66307929b1912e87d
tolseone <asemisynov@mail.ru>
2024-03-15 15:33:32 +0300

    fixed

--- internal/domain/order.go
+++ internal/domain/order.go
@@ -6,6 +6,6 @@ type Order struct {
 	HotelID   string    `json:"hotel_id" validate:"required"`
 	RoomID    string    `json:"room_id" validate:"required"`
 	UserEmail string    `json:"email" validate:"required,email"`
-	From      time.Time `json:"from" validate:"required,time"`
-	To        time.Time `json:"to" validate:"required,time"`
+	From      time.Time `json:"from"`
+	To        time.Time `json:"to"`
 }
